aa766ed03995556af2d3bca717e11d6b2e939087
ArturoOchoa <flavioarturoo@gmail.com>
2024-02-14 13:15:24 -0500

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hola guapo");
+import console from 'console';
+let numA = 30
+let numB = 20
+
+let res = numA + numB
+console.log(res)
+function SALUDO(NOMBRE) {
+    console.log(NOMBRE)
+}
+SALUDO("lucas")
